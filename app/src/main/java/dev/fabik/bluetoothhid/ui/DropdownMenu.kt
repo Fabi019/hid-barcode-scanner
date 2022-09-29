13845ced00c0b075cd2a76e5fb85dbf53d7d78ae
Fabi019 <fabi019@gmx.de>
2022-09-29 12:20:12 +0200

    Fix DropdownMenu position

--- app/src/main/java/dev/fabik/bluetoothhid/ui/DropdownMenu.kt
+++ app/src/main/java/dev/fabik/bluetoothhid/ui/DropdownMenu.kt
@@ -1,7 +1,7 @@
 package dev.fabik.bluetoothhid.ui
 
 import android.app.Activity
-import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.*
@@ -11,31 +11,33 @@ import androidx.navigation.NavHostController
 
 @Composable
 fun Dropdown(
-    navHostController: NavHostController,
-    activity: Activity = LocalContext.current as Activity
+    navHostController: NavHostController
 ) {
+    val activity: Activity = LocalContext.current as Activity
+
     var showMenu by remember {
         mutableStateOf(false)
     }
 
-    IconButton(onClick = { showMenu = !showMenu }) {
-        Icon(
-            imageVector = Icons.Default.MoreVert,
-            contentDescription = "More"
-        )
-    }
+    Box {
+        IconButton(onClick = { showMenu = !showMenu }) {
+            Icon(Icons.Default.MoreVert, "More")
+        }
 
-    DropdownMenu(
-        expanded = showMenu,
-        onDismissRequest = { showMenu = false }) {
-        DropdownMenuItem(
-            text = { Text("Settings") },
-            onClick = {
-                navHostController.navigate(Routes.Settings)
-                showMenu = false
-            })
-        DropdownMenuItem(
-            text = { Text("Exit") },
-            onClick = { activity.finishAfterTransition() })
+        DropdownMenu(
+            expanded = showMenu,
+            onDismissRequest = { showMenu = false }) {
+            DropdownMenuItem(
+                text = { Text("Settings") },
+                onClick = {
+                    navHostController.navigate(Routes.Settings)
+                    showMenu = false
+                }
+            )
+            DropdownMenuItem(
+                text = { Text("Exit") },
+                onClick = { activity.finishAfterTransition() }
+            )
+        }
     }
 }
