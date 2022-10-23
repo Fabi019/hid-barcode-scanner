74ab21d72c8b0c496f30f98a0683d0588afc7fe3
Fabi019 <fabi019@gmx.de>
2022-10-23 13:19:18 +0200

    Refactor scanner

--- app/src/main/java/dev/fabik/bluetoothhid/Scanner.kt
+++ app/src/main/java/dev/fabik/bluetoothhid/Scanner.kt
@@ -20,10 +20,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
-import androidx.compose.ui.platform.LocalClipboardManager
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalHapticFeedback
-import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.platform.*
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.ParagraphStyle
@@ -48,12 +45,47 @@ fun Scanner(
     onDisconnect: () -> Unit,
     sendText: (String) -> Unit
 ) {
-    val context = LocalContext.current
-    val clipboardManager = LocalClipboardManager.current
-    val haptic = LocalHapticFeedback.current
-    val view = LocalView.current
-
     var currentBarcode by rememberSaveable { mutableStateOf<String?>(null) }
+    var camera by remember { mutableStateOf<Camera?>(null) }
+
+    Scaffold(
+        topBar = {
+            ScannerAppBar(camera, onDisconnect, navController)
+        },
+        floatingActionButtonPosition = FabPosition.Center,
+        floatingActionButton = {
+            currentDevice?.let {
+                SendToDeviceFAB(currentBarcode, sendText)
+            }
+        }
+    ) { padding ->
+        Box(
+            Modifier
+                .padding(padding)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            RequiresCameraPermission {
+                CameraPreviewArea({ camera = it }) { value, send ->
+                    currentBarcode = value
+                    if (send) {
+                        sendText(value)
+                    }
+                }
+                BarcodeValue(currentBarcode)
+            }
+            DeviceInfoDialog(currentDevice)
+        }
+    }
+}
+
+@Composable
+private fun CameraPreviewArea(
+    onCameraReady: (Camera) -> Unit,
+    onBarcodeDetected: (String, Boolean) -> Unit,
+) {
+    val view = LocalView.current
+    val haptic = LocalHapticFeedback.current
 
     val playSound by rememberPreferenceDefault(PrefKeys.PLAY_SOUND)
 
@@ -65,94 +97,92 @@ fun Scanner(
 
     val vibrate by rememberPreferenceDefault(PrefKeys.VIBRATE)
 
-    var camera by remember { mutableStateOf<Camera?>(null) }
-
-    Scaffold(topBar = {
-        TopAppBar(title = { Text(stringResource(R.string.scanner)) }, actions = {
-            camera?.let {
-                if (it.cameraInfo.hasFlashUnit()) {
-                    ToggleFlashButton(it)
-                }
-            }
-            IconButton(onDisconnect) {
-                Icon(Icons.Default.BluetoothDisabled, "Disconnect")
-            }
-            Dropdown(navController)
-        })
-    }, floatingActionButtonPosition = FabPosition.Center, floatingActionButton = {
-        currentDevice?.let {
-            currentBarcode?.let {
-                ExtendedFloatingActionButton(onClick = { sendText(it) }) {
-                    Icon(Icons.Filled.Send, "Send")
-                    Spacer(Modifier.width(8.dp))
-                    Text(stringResource(R.string.send_to_device))
-                }
-            }
+    CameraPreview(onCameraReady = onCameraReady) {
+        onBarcodeDetected(it, autoSend)
+        if (playSound) {
+            toneGenerator.startTone(ToneGenerator.TONE_PROP_ACK, 75)
         }
-    }) { padding ->
-        Box(
-            Modifier
-                .padding(padding)
-                .fillMaxSize(), contentAlignment = Alignment.Center
-        ) {
-            RequiresCameraPermission {
-                CameraPreview(onCameraReady = { camera = it }) {
-                    currentBarcode = it
-                    if (playSound) {
-                        toneGenerator.startTone(ToneGenerator.TONE_PROP_ACK, 75)
-                    }
-                    if (autoSend) {
-                        sendText(it)
-                    }
-                    if (vibrate) {
-                        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
-                            @Suppress("DEPRECATION") view.performHapticFeedback(
-                                HapticFeedbackConstants.LONG_PRESS,
-                                HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING
-                            )
-                        } else {
-                            // Might not always work
-                            haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                        }
-                    }
-                }
-
-                Column(
-                    Modifier
-                        .fillMaxHeight(0.3f)
-                        .fillMaxWidth()
-                        .align(Alignment.TopStart),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    currentBarcode?.let {
-                        val text = AnnotatedString(
-                            it,
-                            SpanStyle(Neutral95),
-                            ParagraphStyle(TextAlign.Center)
-                        )
-                        ClickableText(
-                            text, maxLines = 6, overflow = TextOverflow.Ellipsis
-                        ) {
-                            clipboardManager.setText(text)
-                            Toast.makeText(context, "Copied to clipboard!", Toast.LENGTH_SHORT)
-                                .show()
-                        }
-                    } ?: run {
-                        Text(
-                            stringResource(R.string.scan_code_to_start),
-                            color = Neutral95,
-                            textAlign = TextAlign.Center
-                        )
-                    }
-                }
-            }
-
-            currentDevice?.let {
-                DeviceInfoDialog(it)
+        if (vibrate) {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+                @Suppress("DEPRECATION") view.performHapticFeedback(
+                    HapticFeedbackConstants.LONG_PRESS,
+                    HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING
+                )
+            } else {
+                // Might not always work
+                haptic.performHapticFeedback(HapticFeedbackType.LongPress)
             }
         }
     }
+}
+
+@Composable
+private fun BoxScope.BarcodeValue(currentBarcode: String?) {
+    val context = LocalContext.current
+    val clipboardManager = LocalClipboardManager.current
+
+    Column(
+        Modifier
+            .fillMaxHeight(0.3f)
+            .fillMaxWidth()
+            .align(Alignment.TopStart),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val copiedString = stringResource(R.string.copied_to_clipboard)
+
+        currentBarcode?.let {
+            val text = AnnotatedString(it, SpanStyle(Neutral95), ParagraphStyle(TextAlign.Center))
+            ClickableText(
+                text,
+                maxLines = 6,
+                overflow = TextOverflow.Ellipsis
+            ) {
+                clipboardManager.setText(text)
+                Toast.makeText(context, copiedString, Toast.LENGTH_SHORT).show()
+            }
+        } ?: run {
+            Text(
+                stringResource(R.string.scan_code_to_start),
+                color = Neutral95,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+@Composable
+private fun SendToDeviceFAB(
+    currentBarcode: String?,
+    onClick: (String) -> Unit
+) {
+    currentBarcode?.let {
+        ExtendedFloatingActionButton(onClick = { onClick(it) }) {
+            Icon(Icons.Filled.Send, "Send")
+            Spacer(Modifier.width(8.dp))
+            Text(stringResource(R.string.send_to_device))
+        }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun ScannerAppBar(
+    camera: Camera?,
+    onDisconnect: () -> Unit,
+    navController: NavController
+) {
+    TopAppBar(title = { Text(stringResource(R.string.scanner)) }, actions = {
+        camera?.let {
+            if (it.cameraInfo.hasFlashUnit()) {
+                ToggleFlashButton(it)
+            }
+        }
+        IconButton(onDisconnect) {
+            Icon(Icons.Default.BluetoothDisabled, "Disconnect")
+        }
+        Dropdown(navController)
+    })
 }
 
 @Composable
@@ -179,83 +209,85 @@ fun ToggleFlashButton(camera: Camera) {
 @Suppress("MissingPermission")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BoxScope.DeviceInfoDialog(device: BluetoothDevice) {
+fun BoxScope.DeviceInfoDialog(device: BluetoothDevice?) {
     val dialogState = rememberDialogState()
 
-    ElevatedCard(
-        onClick = {
-            dialogState.open()
-        },
-        Modifier
-            .padding(12.dp)
-            .align(Alignment.TopCenter)
-    ) {
-        Row(
-            Modifier.padding(8.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            verticalAlignment = Alignment.CenterVertically
+    device?.let {
+        ElevatedCard(
+            onClick = {
+                dialogState.open()
+            },
+            Modifier
+                .padding(12.dp)
+                .align(Alignment.TopCenter)
         ) {
-            Icon(Icons.Default.Info, "Info")
-            Text(stringResource(R.string.connected_with, device.name))
+            Row(
+                Modifier.padding(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(Icons.Default.Info, "Info")
+                Text(stringResource(R.string.connected_with, it.name))
+            }
         }
-    }
 
-    InfoDialog(dialogState, stringResource(R.string.info), onDismiss = { close() }) {
-        LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-            item {
-                Text(stringResource(R.string.name) + ":", fontWeight = FontWeight.Bold)
-                Text(device.name)
-            }
-
-            item {
-                Text(stringResource(R.string.mac_address) + ":", fontWeight = FontWeight.Bold)
-                Text(device.address)
-            }
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        InfoDialog(dialogState, stringResource(R.string.info), onDismiss = { close() }) {
+            LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)) {
                 item {
-                    Text(stringResource(R.string.alias) + ":", fontWeight = FontWeight.Bold)
-                    Text(device.alias ?: "")
+                    Text(stringResource(R.string.name) + ":", fontWeight = FontWeight.Bold)
+                    Text(it.name)
                 }
-            }
 
-            item {
-                Text(stringResource(R.string.type) + ":", fontWeight = FontWeight.Bold)
-                Text(
-                    when (device.type) {
-                        BluetoothDevice.DEVICE_TYPE_DUAL -> "Dual"
-                        BluetoothDevice.DEVICE_TYPE_LE -> "LE"
-                        BluetoothDevice.DEVICE_TYPE_CLASSIC -> "Classic"
-                        BluetoothDevice.DEVICE_TYPE_UNKNOWN -> "Unknown"
-                        else -> "?"
-                    } + " (${device.type})"
-                )
-            }
+                item {
+                    Text(stringResource(R.string.mac_address) + ":", fontWeight = FontWeight.Bold)
+                    Text(it.address)
+                }
 
-            item {
-                Text(stringResource(R.string.clazz) + ":", fontWeight = FontWeight.Bold)
-                with(device.bluetoothClass.majorDeviceClass) {
-                    val classString = remember(device) {
-                        deviceClassString(this)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    item {
+                        Text(stringResource(R.string.alias) + ":", fontWeight = FontWeight.Bold)
+                        Text(it.alias ?: "")
                     }
-                    Text("$classString (${this})")
                 }
-            }
 
-            item {
-                Text(stringResource(R.string.services) + ":", fontWeight = FontWeight.Bold)
-                val serviceInfo = remember(device) {
-                    deviceServiceInfo(device.bluetoothClass)
+                item {
+                    Text(stringResource(R.string.type) + ":", fontWeight = FontWeight.Bold)
+                    Text(
+                        when (it.type) {
+                            BluetoothDevice.DEVICE_TYPE_DUAL -> "Dual"
+                            BluetoothDevice.DEVICE_TYPE_LE -> "LE"
+                            BluetoothDevice.DEVICE_TYPE_CLASSIC -> "Classic"
+                            BluetoothDevice.DEVICE_TYPE_UNKNOWN -> "Unknown"
+                            else -> "?"
+                        } + " (${it.type})"
+                    )
                 }
-                serviceInfo.forEach {
-                    Text(it)
-                }
-            }
 
-            item {
-                Text(stringResource(R.string.uuids) + ":", fontWeight = FontWeight.Bold)
-                device.uuids.forEach {
-                    Text(it.toString())
+                item {
+                    Text(stringResource(R.string.clazz) + ":", fontWeight = FontWeight.Bold)
+                    with(it.bluetoothClass.majorDeviceClass) {
+                        val classString = remember(it) {
+                            deviceClassString(this)
+                        }
+                        Text("$classString (${this})")
+                    }
+                }
+
+                item {
+                    Text(stringResource(R.string.services) + ":", fontWeight = FontWeight.Bold)
+                    val serviceInfo = remember(it) {
+                        deviceServiceInfo(it.bluetoothClass)
+                    }
+                    serviceInfo.forEach {
+                        Text(it)
+                    }
+                }
+
+                item {
+                    Text(stringResource(R.string.uuids) + ":", fontWeight = FontWeight.Bold)
+                    it.uuids.forEach {
+                        Text(it.toString())
+                    }
                 }
             }
         }
