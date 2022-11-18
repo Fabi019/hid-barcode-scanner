efca94bb5f31fe5bb8db90f2fbca622adee4a8ab
Fabi019 <fabi019@gmx.de>
2022-11-18 09:50:06 +0100

    Improve ButtonPreference component

--- app/src/main/java/dev/fabik/bluetoothhid/ui/Preference.kt
+++ app/src/main/java/dev/fabik/bluetoothhid/ui/Preference.kt
@@ -1,17 +1,15 @@
 package dev.fabik.bluetoothhid.ui
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.stateDescription
-import androidx.compose.ui.unit.dp
-import dev.fabik.bluetoothhid.ui.theme.Typography
 import dev.fabik.bluetoothhid.utils.PreferenceStore
 import dev.fabik.bluetoothhid.utils.rememberPreferenceNull
 
@@ -25,39 +23,17 @@ fun ButtonPreference(
     extra: (@Composable () -> Unit)? = null,
     onClick: () -> Unit = {}
 ) {
-    ElevatedCard(
-        onClick,
-        shape = MaterialTheme.shapes.small,
+    ListItem(
         modifier = Modifier
-            .fillMaxWidth()
-            .heightIn(56.dp)
-    ) {
-        Row(
-            Modifier
-                .padding(4.dp)
-                .heightIn(48.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Box(
-                Modifier.size(40.dp),
-                contentAlignment = Alignment.Center
-            ) {
-                icon?.let {
-                    Icon(icon, null)
-                }
-            }
-            Column(Modifier.weight(1f)) {
-                Text(title)
-                Text(
-                    desc,
-                    color = MaterialTheme.colorScheme.onSurfaceVariant,
-                    style = Typography.labelSmall,
-                    softWrap = true
-                )
-            }
-            extra?.invoke()
-        }
-    }
+            .semantics { stateDescription = desc }
+            .clickable(onClick = onClick),
+        leadingContent = icon?.let {
+            { Icon(icon, contentDescription = null) }
+        },
+        headlineText = { Text(title) },
+        supportingText = { Text(desc) },
+        trailingContent = extra
+    )
 }
 
 @Composable
