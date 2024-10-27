78e64aa5ea055b6d9f4405384ab8d46db9007a12
Fabi019 <fabi019@gmx.de>
2024-10-27 10:30:58 +0100

    Update dependencies

--- app/src/main/java/dev/fabik/bluetoothhid/ui/Tooltip.kt
+++ app/src/main/java/dev/fabik/bluetoothhid/ui/Tooltip.kt
@@ -14,9 +14,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.DropdownMenuPositionProvider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.internal.DropdownMenuPositionProvider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
