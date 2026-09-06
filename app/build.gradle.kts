d04a7ceadc0e7d8e9837331470bf46c74ddaf6ac
Fabi019 <fabi019@gmx.de>
2026-09-06 13:25:44 +0200

    Enable auto-generation of locale config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,10 @@ android {
         }
     }
 
+    androidResources {
+        generateLocaleConfig = true
+    }
+
     dependenciesInfo {
         includeInApk = false
         includeInBundle = false
