85d76c37f31aeb8a84044caa0a1dd8c485c4dd33
Fabi019 <fabi019@gmx.de>
2026-09-13 15:23:09 +0200

    Update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "dev.fabik.bluetoothhid"
         minSdk = 28
         targetSdk = 37
-        versionCode = 59
-        versionName = "2.3.1"
+        versionCode = 60
+        versionName = "2.3.2"
 
         buildConfigField("String", "GIT_COMMIT_HASH", "\"$gitHash\"")
 
